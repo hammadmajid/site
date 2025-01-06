2651b7156cbf5f0d15e33767e4fc9891d5174ce8
Hammad Majid <hammadmajid@proton.me>
2025-01-06 22:54:43 +0500

    Change name, links and meta info
    also format code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,68 +1,71 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ViewTransitions } from 'next-view-transitions';
-import { Analytics } from '@vercel/analytics/react';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ViewTransitions } from "next-view-transitions";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://leerob.com'),
-  alternates: {
-    canonical: '/',
-  },
-  title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
-  },
-  description: 'Frontend developer, optimist, community builder.',
+	metadataBase: new URL("https://leerob.com"),
+	alternates: {
+		canonical: "/",
+	},
+	title: {
+		default: "Hammad Majid",
+		template: "%s | Hammad Majid",
+	},
+	description: "Frontend developer and a student.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ViewTransitions>
-      <html lang="en" className={`${inter.className}`}>
-        <body className="antialiased tracking-tight">
-          <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
-            <main className="max-w-[60ch] mx-auto w-full space-y-6">
-              {children}
-            </main>
-            <Footer />
-            <Analytics />
-          </div>
-        </body>
-      </html>
-    </ViewTransitions>
-  );
+	return (
+		<ViewTransitions>
+			<html lang="en" className={`${inter.className}`}>
+				<body className="antialiased tracking-tight">
+					<div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
+						<main className="max-w-[60ch] mx-auto w-full space-y-6">
+							{children}
+						</main>
+						<Footer />
+						<Analytics />
+					</div>
+				</body>
+			</html>
+		</ViewTransitions>
+	);
 }
 
 function Footer() {
-  const links = [
-    { name: '@leerob', url: 'https://x.com/leeerob' },
-    { name: 'youtube', url: 'https://www.youtube.com/@leerob' },
-    { name: 'linkedin', url: 'https://www.linkedin.com/in/leeerob' },
-    { name: 'github', url: 'https://github.com/leerob' },
-  ];
+	const links = [
+		{
+			name: "@hammadmajid",
+			url: "https://bsky.app/profile/hammadmajid.bsky.social",
+		},
+		{ name: "mastodon", url: "https://mastodon.social/@hammadmajid" },
+		{ name: "linkedin", url: "https://www.linkedin.com/in/hammadmajid" },
+		{ name: "github", url: "https://github.com/hammadmajid" },
+	];
 
-  return (
-    <footer className="mt-12 text-center">
-      <div className="flex justify-center space-x-4 tracking-tight">
-        {links.map((link) => (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 hover:text-blue-500 transition-colors duration-200"
-          >
-            {link.name}
-          </a>
-        ))}
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="mt-12 text-center">
+			<div className="flex justify-center space-x-4 tracking-tight">
+				{links.map((link) => (
+					<a
+						key={link.name}
+						href={link.url}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-gray-400 hover:text-blue-500 transition-colors duration-200"
+					>
+						{link.name}
+					</a>
+				))}
+			</div>
+		</footer>
+	);
 }
