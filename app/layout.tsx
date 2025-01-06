555367d5bec1651e8a4ceb65d0c76a2e1a31fc50
Hammad Majid <hammadmajid@proton.me>
2025-01-06 23:05:28 +0500

    Add Umami script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { IBM_Plex_Mono } from "next/font/google";
 import { ViewTransitions } from "next-view-transitions";
+import Script from "next/script";
 
 const IBMPlexMono = IBM_Plex_Mono({
 	subsets: ["latin"],
@@ -29,6 +30,13 @@ export default function RootLayout({
 	return (
 		<ViewTransitions>
 			<html lang="en" className={`${IBMPlexMono.className}`}>
+				<head>
+					<Script
+						defer
+						src="https://umami.bine.codes/script.js"
+						data-website-id="0b581d53-f6b5-4131-9c8b-cffaafeebe16"
+					/>
+				</head>
 				<body className="antialiased tracking-tight">
 					<div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
 						<main className="max-w-[60ch] mx-auto w-full space-y-6">
